6e5ff1be1ce2eece4acf36771ca87684e606cd87
yurasera <yuhaya.lissera@gmail.com>
2026-06-23 21:34:19 +0700

    Initial Commit

---
+++ Flocus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flocus
+//
+//  Created by Yuhaya Lissera on 23/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flocus/FlocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlocusApp.swift
+//  Flocus
+//
+//  Created by Yuhaya Lissera on 23/06/26.
+//
+
+import SwiftUI
+
+@main
+struct FlocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
